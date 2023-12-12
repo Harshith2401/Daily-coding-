7c392d58b2286dccf21a7d4fe19a3cdacc1092f6
Harshith2401 <orugantiharshith@gmail.com>
2023-12-12 19:47:53 +0530

    first code

---
+++ arr.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main()
+{
+	int a1[]={1,2,3,4};
+	int a2[]={};
+	int i,size,j;
+	size=sizeof(a1)/sizeof(a1[0])
+	for(i=0,j=0;i,j<size;i++,j++)
+	{
+		a2[j]=a1[i]*-1;
+	}
+	for(j=0;j<size;j++)
+	{
+		printf("%d ",a2[j]);
+	}
+	return 0;
+
+	// vish-harsh
+}

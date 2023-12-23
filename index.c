0c1ba1e4e65920e9d541e1bb2278bf8216383eaf
Harshith2401 <orugantiharshith@gmail.com>
2023-12-23 21:45:46 +0530

    sum is k and we have to print index which is =k

---
+++ index.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main()
+{
+  int k=6;
+  int arr[]={1,2,3,4,5};
+  int size=sizeof(arr)/sizeof(arr[0]);
+  for(int i=0;i<=size-1;i++)
+    {
+      for(int j=i+1;j<size;j++)
+        {
+          if(j<=i)
+          {
+            continue;
+          }
+          if(arr[i]+arr[j]==k)
+          {
+            printf("%d %d\n",i,j);
+          }
+        }
+    }
+  return 0;
+}

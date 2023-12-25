ac12f433fd9c8baf3fa4a3d1807cda606e79d140
Harshith2401 <orugantiharshith@gmail.com>
2023-12-25 19:19:22 +0530

    frequency of letters

--- frequencyofletters.c
+++ frequencyofletters.c
@@ -1,26 +1,35 @@
+/*Question:
+Given an array of characters (only small a to z) print the frequency of each character present in the array.
+Example :
+A = [ a, b, c, b, a]
+Output: 
+Freq of a is 2
+Freq of b is 2
+Freq of c is 1*/
 #include<stdio.h>
 #include<stdlib.h>
-int main()
-{
-    char arr[]={'a','b','c','b','a'};
-    int size=sizeof(arr)/sizeof(arr[0]);
-    char s='@';
-    if(size<=0)
-    {
+
+int main() {
+    char arr[] = {'a', 'b', 'c', 'b', 'a'};
+    int size = sizeof(arr) / sizeof(arr[0]);
+
+    if (size <= 0) {
         return 0;
     }
-    for(int i=0,j=j+1;i<size||j<size;i++)
-    {
-        int frequency=1;
-        if(arr[i]!='@')
-        {
-            if(arr[i]==arr[j])
-                {
+
+    for (int i = 0; i < size; i++) {
+        int frequency = 1;
+
+        if (arr[i] != '@') {
+            for (int j = i + 1; j < size; j++) {
+                if (arr[i] == arr[j]) {
                     frequency++;
-                    arr[j]='@';
+                    arr[j] = '@';
                 }
-             printf("the frequency of %c is %d\n",arr[i],frequency);
+            }
+            printf("The frequency of %c is %d\n", arr[i], frequency);
         }
     }
+
     return 0;
 }

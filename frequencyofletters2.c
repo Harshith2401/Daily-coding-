4c8d52d4ba3f851f2903dcc860c504f53cb234dc
Harshith2401 <orugantiharshith@gmail.com>
2023-12-25 19:00:27 +0530

    another most efficient way

---
+++ frequencyofletters2.c
@@ -0,0 +1,28 @@
+/*Question:
+Given an array of characters (only small a to z) print the frequency of each character present in the array.
+Example :
+A = [ a, b, c, b, a]
+Output: 
+Freq of a is 2
+Freq of b is 2
+Freq of c is 1*/
+#include<stdio.h>
+#include<stdlib.h>
+int main()
+{
+    char arr[]={'a','b','c','a','c'};
+    int frequency[26]={0};                  //creted a extra array of 26 size and each is initialised by0
+    int size=sizeof(arr)/sizeof(arr[0]);
+    for( int i=0;i<size;i++)
+    {
+        frequency[arr[i]-'a']++;            //this is to locate the array of frequency and increse the frequency a is used as ascci values.
+    }
+    for(int i=0;i<26;i++)
+    {
+        if(frequency[i]>0)
+        {
+            printf("%c:%d\n",i+'a',frequency[i]);
+        }
+    }
+    return 0;
+}

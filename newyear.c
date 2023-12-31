918a3c1886e1ccc24dfdcf11fded0cdf201888da
Harshith2401 <orugantiharshith@gmail.com>
2023-12-31 23:15:35 +0530

    still lot to climb keep going with atmost honesty

---
+++ newyear.c
@@ -0,0 +1,10 @@
+/*Qsn: 
+Just print this string : 
+" Happy new year 2024 Fellas, let's rock this year"*/
+#include<stdio.h>
+#include<stdlib.h>
+int main()
+{
+    printf("Happy new year 2024 Fellas, let's rock this year");
+    return 0;
+}

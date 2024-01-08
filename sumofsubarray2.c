9b67ed8ab3275eb7ac883b4e67df96fab6d1d8f5
Harshith2401 <orugantiharshith@gmail.com>
2024-01-08 14:59:29 +0530

    updated

--- sumofsubarray2.c
+++ sumofsubarray2.c
@@ -1,9 +1,20 @@
+/*Qsn) Given an array , find if there's a sub array of size any thing with sum of elements = 15. Return true or false. 
+
+Eg: A = [ 1,-1, 4, 5, 6, 7, -5] 
+Output: true 
+
+Explanation : [ 4,5,6] subarray has sum 15*/
 #include<stdio.h>
 #include<stdlib.h>
 int main()
 {
     int nums[]={0,0,0};
     int size=sizeof(nums)/sizeof(nums[0]);
+    if(size<=0)
+    {
+        printf("empty array");
+        return 0;
+    }
     int k=5;
 
     // outer loop is responsible for start index.
@@ -13,6 +24,7 @@ int main()
 
         // inner loop is responsible for end index
         for(int j=i;j<size;j++)
+
         {
             sum+=nums[j];
             if(sum==k)

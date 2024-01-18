9cad9b3e38453f26aa66fafa2b4728c480959d46
Harshith2401 <orugantiharshith@gmail.com>
2024-01-18 22:58:06 +0530

    IMPORTANT BS AND IS

---
+++ insertionbinary.c
@@ -0,0 +1,77 @@
+#include<stdio.h>
+#include<stdlib.h>
+int binary_search(int nums[],int size,int target);
+void insertion_sort(int nums[],int size);
+int main()
+{
+    int nums[]={1,2,9,75,5,6};
+    int target=5;
+    int size=sizeof(nums)/sizeof(nums[0]);
+    if(size<=0)
+    {
+        printf("empty array");
+        return 0;
+    }
+    insertion_sort(nums,size);
+
+    // assiging binary search result to result
+    int result=binary_search(nums,size,target);
+    if(result!=-1)
+    {
+        printf("true");
+    }
+    else{
+        printf("false ");
+    }
+    return 0;
+}
+
+// binary search function
+int binary_search(int nums[],int size,int target)
+{
+    int low=0,high=size-1;
+    int f=-1;
+
+
+    while(low<=high)
+    {
+        int mid=(low+high)/2;// finding mid.
+        int midvalue=nums[mid];
+        if(midvalue==target)
+        {
+            return mid;
+        }
+
+        // updating low and high accordingly.
+        else if(midvalue<target)
+        {
+            low=mid+1;
+        }
+        else
+        {
+            high=mid-1;
+        }    
+    }
+    return -1;
+}
+
+// insertion sort.
+void insertion_sort(int nums[],int size)
+{
+    for(int i=1;i<size;i++)
+    {
+        // assigning of the key value to be inserted in correct postion.
+        int k=nums[i];
+        int j=i-1;
+
+        // move all greater elements to one position ahed.
+        while(j>=0&&nums[j]>k)
+        {
+            nums[j+1]=nums[j];
+            j--;
+        }
+
+        // place the key element in  its right position.
+        nums[j+1]=k;
+    }
+}

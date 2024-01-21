8eca4fe820a10571290e64ce15e20dc2c5d7bc41
Harshith2401 <orugantiharshith@gmail.com>
2024-01-21 21:55:43 +0530

    roman to numbers

---
+++ romantonumbers.c
@@ -0,0 +1,66 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main()
+{
+    char roman[]="IL";
+    int num=0;
+    for(int i=0;roman[i]!='\0';i++)
+    {
+        switch(roman[i])
+        {
+            case 'I':
+            if(roman[i+1]=='V'||roman[i+1]=='X')
+            {
+                num-=1;
+            }
+            else
+            {
+                num+=1;
+            }
+            break;
+
+            case 'V':
+            num+=5;
+            break;
+
+            case 'X':
+            if(roman[i+1]=='L'||roman[i+1]=='C')
+            {
+                num-=10;
+            }
+            else
+            {
+                num+=10;
+            }
+            break;
+
+            case 'L':
+            num+=50;
+            break;
+
+            case 'C':
+            if(roman[i+1]=='D'||roman[i+1]=='M')
+            {
+                num-=100;
+            }
+            else
+            {
+                num+=100;
+            }
+            break;
+
+            case 'D':
+            num+=500;
+            break;
+
+            case 'M':
+            num+=1000;
+            break;
+            default :
+            printf("invalid");
+            return 1;
+        }
+    }
+    printf("%d",num);
+    return 0;
+}

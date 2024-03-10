a7185fa5c81662b4bb452b2ae27a72dbb4df47b7
Harshith2401 <orugantiharshith@gmail.com>
2024-03-10 23:08:14 +0530

    deleting duplicate nodes

--- deleteduplicatesinll.c
+++ deleteduplicatesinll.c
@@ -13,20 +13,20 @@ void duplicate(struct node **start );
 void display(struct node *start);
 int main()
 {
-    int nums[]={1,1,1,1,1,1,1,10,0,0,-1};
+    int nums[]={1,1,2,2,3,3,4,-1};
     int size=sizeof(nums)/sizeof(nums[0]);
     struct node*start=NULL;
     struct node*end=NULL;
-    start=create(start,end,nums,size);
-    duplicate(&start);
-    display(start);
+    start=create(start,end,nums,size);//tc of create()
+    duplicate(&start);// tc of duplicate()
+    display(start);//tc of display.
     return 0;
 }
 // function to create the list.
 struct node *create(struct node *start,struct node *end,int nums[],int size)
 {
     struct node*new_node,*ptr;
-    for(int i=0;i<size;i++)
+    for(int i=0;i<size;i++)//TC=O(n)
     {
         if(nums[i]==-1)
         {
@@ -49,15 +49,15 @@ struct node *create(struct node *start,struct node *end,int nums[],int size)
     return start;
 }
 // function to delete the duplicates.
-void duplicate(struct node **start)
+void duplicate(struct node **start)//TC of the function=O(n^2)
 {
     struct node *ptr=*start,*prev,*dup;
-    while(ptr!=NULL&&ptr->next!=NULL)// outer loop for anchor pointer.
+    while(ptr!=NULL&&ptr->next!=NULL)// outer loop for anchor pointer.// TC=O(n)
     {
         prev=ptr;// asigined to prev to make iteration easy.
         while(prev->next!=NULL)// inner loop to check the data part of anchor is there.
         {
-            if(ptr->data==prev->next->data)
+            if(ptr->data==prev->next->data)//TC=O(n)
             {
                 dup=prev->next;// asigining the node dup.
                 prev->next=prev->next->next;// moving the pointers to forward.
@@ -71,13 +71,14 @@ void duplicate(struct node **start)
         ptr=ptr->next;// iterating the anchor pointer.
     }
 }
-void display(struct node *start)
+void display(struct node *start)//TC of the function=o(n)
 {
     struct node*ptr;
     ptr=start;
-    while(ptr!=NULL)
+    while(ptr!=NULL)//TC=O(n)
     {
         printf("%d ",ptr->data);
         ptr=ptr->next;
     }
 }
+//time complexity =O(n)+O(n)+O(n^2)===O(N^2)

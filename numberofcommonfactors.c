1b70ba7e11e79d8272164b85e31df8fa89c7609a
Harshith2401 <orugantiharshith@gmail.com>
2024-03-12 00:36:09 +0530

    number of common factors

--- numberofcommonfactors.c
+++ numberofcommonfactors.c
@@ -1,3 +1,4 @@
+//find out the number of common factors.
 #include<stdio.h>
 #include<stdlib.h>
 int main()
@@ -8,7 +9,7 @@ int main()
     if(num1==0||num2==0)
     {
         int greater=num1>num2?num1:num2; //;// compact way to write if else(ternaray operator)
-        for(int i=1;i<=greater;i++)
+        for(int i=1;i<=greater;i++)//TC=O(g)
         {
             if(greater%i==0)
             {
@@ -20,7 +21,7 @@ int main()
     }
 
     int smaller=num1<num2?num1:num2;// compact way to write if else(ternaray operator)
-    for( int i=1;i<=smaller;i++)
+    for( int i=1;i<=smaller;i++)//TC=O(s)
     {
         if(num1%i==0&&num2%i==0)
         {
@@ -30,3 +31,5 @@ int main()
     printf("%d",count);
     return 0;
 }
+//TIMECOMPLIXIXTY=O(n)
+

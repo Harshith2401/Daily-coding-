6a7d97da8eae781dde5176b596952b1b04b862ed
Harshith2401 <orugantiharshith@gmail.com>
2024-03-12 00:01:59 +0530

    finding out the natural number in n natural numbers

--- missingnum.c
+++ missingnum.c
@@ -1,3 +1,4 @@
+//FINDING OUT MISSING NATURAL NUMBER 
 #include<stdio.h>
 #include<stdlib.h>
 int main()
@@ -8,14 +9,15 @@ int main()
         return 0;
     int n=size+1;
     int tsum=0,sum=0;
-    for(int j=1;j<=n;j++)
+    for(int j=1;j<=n;j++)//TC=O(n)
     {
         tsum=tsum+j;
     }
-    for(int i=0;i<size;i++)
+    for(int i=0;i<size;i++)//TC=O(n)
     {
         sum=sum+arr[i];
     }
     printf("%d",tsum-sum);
     return 0;
 }
+//TIECOMPLIXIXTY=O(n)+O(n)=O(2n)= O(n)

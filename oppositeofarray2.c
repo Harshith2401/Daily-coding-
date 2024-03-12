b07df899f0b6bfdddabc479f38336b46e9c01d60
Harshith2401 <orugantiharshith@gmail.com>
2024-03-12 23:30:23 +0530

    making whole array sign reverse

--- oppositeofarray2.c
+++ oppositeofarray2.c
@@ -7,13 +7,14 @@ int main()
 	int a2[]={};
 	int i,size,j;
 	size=sizeof(a1)/sizeof(a1[0]);
-	for(i=0,j=0;i,j<size;i++,j++)
+	for(i=0,j=0;i,j<size;i++,j++)//TC=O(n)
 	{
 		a2[j]=a1[i]*-1;
 	}
-	for(j=0;j<size;j++)
+	for(j=0;j<size;j++)//TC=O(n)
 	{
 		printf("%d ",a2[j]);
 	}
 	return 0;
 }
+//TIMECOMPLEXITY=O(n)+O(n)=O(2n)= O(n)

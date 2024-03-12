9f7ba8f118916fdd96a1e9704802ddfa4157d6f6
Harshith2401 <orugantiharshith@gmail.com>
2024-03-13 00:10:26 +0530

    printing only alphabets from string

--- printalphabet.c
+++ printalphabet.c
@@ -1,10 +1,11 @@
+//PRINT ONLY ALPHABETS FROM THE STRING.
 #include<stdio.h>
 #include<stdlib.h>
 int main()
 {
     char arr[]={"hello,how are you?"};
     int i=0;
-    while(arr[i]!='\0')
+    while(arr[i]!='\0')//TC=O(n)
     {
         if((arr[i]>='a'&&arr[i]<='z')||(arr[i]>='A'&&arr[i]<='Z'))
         {
@@ -13,3 +14,4 @@ int main()
         i++;
     }
 }
+//TIMECOMPLIXIXTY=O(n)

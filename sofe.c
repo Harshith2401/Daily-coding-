e142d9b5d317df919fff6d1f1db5d43fd6405ad3
Harshith2401 <orugantiharshith@gmail.com>
2024-03-13 00:45:17 +0530

    Sieve of Eratosthenes algorithm for prime numbers upto limit

--- sofe.c
+++ sofe.c
@@ -6,17 +6,17 @@ int main()
     int flag[num + 1];
 
     // initializing the array to 1
-    for(int i=0;i<=num;i++)
+    for(int i=0;i<=num;i++)//TC=O(n)
     {
         flag[i]=1;
     }
 
     // SIEVE OF ERATOSTHENES.
-    for(int i=2;i*i<=num;i++)
+    for(int i=2;i*i<=num;i++)//TC=O(sqrt(n))
     {
         if(flag[i] == 1)
         {
-            for(int j=i*i;j<=num;j+=i)
+            for(int j=i*i;j<=num;j+=i)//TC=O(logn)
             {
                 flag[j]=0;
             }
@@ -32,4 +32,5 @@ int main()
         }
     }
     return 0;
+    //TIMECOMPLIXIXTY=O(sqrt(m)*logn)
 }

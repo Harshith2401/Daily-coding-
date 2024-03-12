50b9dfedde3a41b674815286f69c6780b0d957f9
Harshith2401 <orugantiharshith@gmail.com>
2024-03-12 23:44:49 +0530

    printing all possible permutations

--- permutations.c
+++ permutations.c
@@ -1,3 +1,4 @@
+//writing all permutataions possible.
 #include<stdio.h>
 #include<stdlib.h>
 void permutation(int arr[],int start,int end);
@@ -28,11 +29,12 @@ int main()
     return 0;
 }
 // function to print the array
+//TC=O(n)
 void printing_array(int arr[],int size)
 {
     for(int i=0;i<size;i++)
     {
-        printf("%d",arr[i]);
+        printf("%d",arr[i]);//TC=O(n)
     }
     printf("\n");
 }
@@ -45,9 +47,10 @@ void swap(int arr[],int i,int j)
 }
 
 // funtion to eliminte the cases where there are same digits in number.
+//TC=O(n)
 int shouldswap(int arr[],int start,int curr)
 {
-    for(int i=start;i<curr;i++)
+    for(int i=start;i<curr;i++)//TC=O(n)
     {
         if(arr[i]==arr[curr])
         {
@@ -62,21 +65,22 @@ void permutation(int arr[],int start,int end)
 {
    if(start==end)//when start==end no need of swaping so seperated it.
    {
-    printing_array(arr,end);
+    printing_array(arr,end);//TC=O(n)
    }
    else
    {
-        for(int i=start;i<end;i++)
+        for(int i=start;i<end;i++)//TC=O(n!)
         {
-            if(shouldswap(arr,start,i))// it will eliminate all duplicates
+            if(shouldswap(arr,start,i)) //TC=O(n) // it will eliminate all duplicates
             {
-                swap(arr,start,i);
+                swap(arr,start,i);//TC=O(1)
                 permutation(arr,start+1,end);// +1 is used for recursion purpose.
-                swap(arr,start,i);
+                swap(arr,start,i);//TC=O(1)
             }
         }
    }
 }
+//TIMECOMPLIXIXTY=O(N*N!)  tentive unable to solve yet.
 
 
 

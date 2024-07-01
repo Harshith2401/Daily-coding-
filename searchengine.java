42481ba26afe7400950b4be5f6aa6465a786c7d9
Harshith2401 <orugantiharshith@gmail.com>
2024-07-01 23:49:45 +0530

    working of searching

---
+++ searchengine.java
@@ -0,0 +1,13 @@
+/*Next up.. interesting question.. on a very high level, this is how Google search works.. 
+Qsn:
+You have 10 documents with text in it. 
+Let's say: 
+Doc A : " Hello, I'm document one. How are you doing..... My writer is khushi"
+Doc B : " ... "
+.
+.
+.. 
+10 such documents...
+Given a query "Khushi writes well" 
+You print the documents in the order of score.
+If Khushi word is present in a particular doc x times, increase doc_score by x.. and keep repeating..*/

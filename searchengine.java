b9e60e3fd1bded4a834884b7e222ccdf12ca558a
Harshith2401 <orugantiharshith@gmail.com>
2024-07-11 23:25:12 +0530

    inprocess

--- searchengine.java
+++ searchengine.java
@@ -11,3 +11,20 @@ Doc B : " ... "
 Given a query "Khushi writes well" 
 You print the documents in the order of score.
 If Khushi word is present in a particular doc x times, increase doc_score by x.. and keep repeating..*/
+class search
+{
+    public static void main(String args[])
+    {
+        String a1="hello iam string one .how are you doing ..my writer is khushi";
+        String a2="hey hello"
+        String a3="hello iam string one .how are you doing ..my writer is harshith";
+        String a4="hello iam string one .how are you doing ..my writer is khushi.khushi is great writer";
+        String a5="hello iam string one .how are you doing ..my writer is khushi,khushi is good girl,khushi as greate taste";
+        String target="khushi is writer";
+        
+
+
+    }
+
+}
+

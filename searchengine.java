1ae9d3a08278e64ad0bb88d78a1b3e48f9851230
Harshith2401 <orugantiharshith@gmail.com>
2024-07-12 23:17:46 +0530

    inprocess

--- searchengine.java
+++ searchengine.java
@@ -11,20 +11,45 @@ Doc B : " ... "
 Given a query "Khushi writes well" 
 You print the documents in the order of score.
 If Khushi word is present in a particular doc x times, increase doc_score by x.. and keep repeating..*/
+import java.util.HashMap;
 class search
 {
     public static void main(String args[])
     {
-        String a1="hello iam string one .how are you doing ..my writer is khushi";
-        String a2="hey hello"
-        String a3="hello iam string one .how are you doing ..my writer is harshith";
-        String a4="hello iam string one .how are you doing ..my writer is khushi.khushi is great writer";
-        String a5="hello iam string one .how are you doing ..my writer is khushi,khushi is good girl,khushi as greate taste";
-        String target="khushi is writer";
-        
-
-
+        String docs[]={"hello iam string one .how are you doing ..my writer is khushi",
+        "hey hello",
+        "hello iam string one .how are you doing ..my writer is harshith",
+        "hello iam string one .how are you doing ..my writer is khushi.khushi is great writer",
+        "hello iam string one .how are you doing ..my writer is khushi,khushi is good girl,khushi as greate taste"};
+        String target="khushi";
+        HashMap<String,Integer>map=new HashMap<>();// creation of hashmap.
+        for(String doc:docs)
+        {
+            int result=Searchof(doc,target);
+            map.put(doc,result);
+        }
+        for (String key : map.keySet()) {
+            System.out.println("Document:\""+ key + "\"- Count of '"+ target+"': "+map.get(key));
+        }
     }
-
+    // method to find the scoe of each string.
+    public static int Searchof(String doc,String target)
+    {
+        int count=0;
+        int index=0;
+        while((index=doc.indexOf(target,index))!=-1)// indexOF is function which checks the target at the given index.
+        {
+            count++;
+            index+=target.length();
+        }
+        return count;
+    }
 }
+//DEEP EXPLANATION OF CODE AS WE USED MANY BUILT IN FUNCTIONS
+/*CONDITION----The loop condition is while ((index = doc.indexOf(target, index)) != -1).
+Here doc.indexOf(target, index) searches for the target string starting from the current index.
+If it finds target it returns the position (index) where it starts in the doc. If it doesnt find it it returns -1. 
 
+If the condition is true (meaning the target was found):
+count++: This increases the count by 1 noting that we found another occurrence of target.
+index += target.length();: This updates index to be just after the last found occurrence of target This way the next search will start right after the found index.*/

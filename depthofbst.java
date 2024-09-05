96dfdbb4149c9027a5708bad6c2cd8cc6631bce0
Harshith2401 <orugantiharshith@gmail.com>
2024-09-05 23:21:16 +0530

    calculating the depth of given bst

---
+++ depthofbst.java
@@ -0,0 +1,59 @@
+/*Depth of Binary Tree
+Solved 
+Given the root of a binary tree, return its depth.
+
+The depth of a binary tree is defined as the number of nodes along the longest path from the root node down to the farthest leaf node.
+
+Example 1:
+
+
+
+Input: root = [1,2,3,null,null,4]
+
+Output: 3
+Example 2:
+
+Input: root = []
+
+Output: 0
+Constraints:
+
+0 <= The number of nodes in the tree <= 100.
+-100 <= Node.val <= 100
+ */
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+ public class Solution {
+    public int maxDepth(TreeNode root) {
+        Stack<Pair<TreeNode, Integer>> stack = new Stack<>();
+        stack.push(new Pair<>(root, 1));
+        int res = 0;
+
+        while (!stack.isEmpty()) {
+            Pair<TreeNode, Integer> current = stack.pop();
+            TreeNode node = current.getKey();
+            int depth = current.getValue();
+
+            if (node != null) {
+                res = Math.max(res, depth);
+                stack.push(new Pair<>(node.left, depth + 1));
+                stack.push(new Pair<>(node.right, depth + 1));
+            }
+        }
+        return res;
+    }
+}

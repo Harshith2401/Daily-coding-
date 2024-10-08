68db1cddcd67653320f4c34b8574e5299d36960a
Harshith2401 <orugantiharshith@gmail.com>
2024-10-08 23:17:16 +0530

    tic tac toe game

---
+++ tictactoe.java
@@ -0,0 +1,30 @@
+/*7 Write a program that will allow two users to play tic-tac-toe. The program should 
+ask for moves alternately from player X and player O. The program displays the 
+game positions as follows:
+1 2 3
+4 5 6
+7 8 9
+The players enter their moves by entering the position number they wish to mark. 
+After each move, the program displays the changed board. A sample board 
+configuration is as follows:
+X X O
+4 5 6
+O 8 9 */
+
+import java.util.Scanner;
+class Tictactoe
+{
+    public static void main(String[] args) 
+    {
+        int nums[][]=new int[3][3];
+        z=0;
+        for(int i=0;i<3;i++)
+        {
+            for(int j=0;j<3;j++)
+            {
+                nums[i][j];
+            }
+        }
+        
+    }
+}

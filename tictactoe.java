483fa1de98a99984e4678956c6705be11964f029
Harshith2401 <orugantiharshith@gmail.com>
2024-10-10 00:10:33 +0530

    tic tac toe game

--- tictactoe.java
+++ tictactoe.java
@@ -16,15 +16,85 @@ class Tictactoe
 {
     public static void main(String[] args) 
     {
-        int nums[][]=new int[3][3];
-        z=0;
-        for(int i=0;i<3;i++)
+        Scanner sc=new Scanner(System.in);
+        String nums[][]=new String[4][4];
+        int z=1;
+        for(int i=1;i<=3;i++)
         {
-            for(int j=0;j<3;j++)
+            for(int j=1;j<=3;j++)
             {
-                nums[i][j];
+                nums[i][j]=String.valueOf(z);
+                z++;
             }
+        }
+        for(int i=1;i<=3;i++)
+        {
+            for(int j=1;j<=3;j++)
+            {
+                System.out.print(nums[i][j]+" ");
+            }
+            System.out.println();
+        }
+        char coin;
+        int turn=1;
+        while(true)
+        {
+            System.out.println("Enter the postion number");
+            int position=sc.nextInt();
+            if(turn%2==0)
+            {
+                coin='O';
+            }
+            else
+            {
+                coin='X';
+            }
+            turn++;
+            switch(position)
+            {
+                case 1:
+                    nums[1][1]=String.valueOf(coin);
+                    break;
+                case 2:
+                    nums[1][2]=String.valueOf(coin);
+                    break;
+                case 3:
+                    nums[1][3]=String.valueOf(coin);
+                    break;
+                case 4:
+                    nums[2][1]=String.valueOf(coin);
+                    break;
+                case 5:
+                    nums[2][2]=String.valueOf(coin);
+                    break;
+                case 6:
+                    nums[2][3]=String.valueOf(coin);
+                    break;
+                case 7:
+                    nums[3][1]=String.valueOf(coin);
+                    break;
+                case 8:
+                    nums[3][2]=String.valueOf(coin);
+                    break;
+                case 9:
+                    nums[3][3]=String.valueOf(coin);
+                    break;
+                 default:
+                    System.out.println("Invalid position! Try again.");
+                    continue;
+            }
+            System.out.println("Board after the move");
+            for(int i=1;i<=3;i++)
+            {
+                for(int j=1;j<=3;j++)
+                {
+                    System.out.print(nums[i][j]+" ");
+                }
+                System.out.println();
+            }
+
         }
         
     }
 }
+

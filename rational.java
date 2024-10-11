8bcf2b98c69ba8e7a920c84e744d6dde3adfeeb5
Harshith2401 <orugantiharshith@gmail.com>
2024-10-11 23:19:14 +0530

    assignent problem

---
+++ rational.java
@@ -0,0 +1,6 @@
+/*Write a Java program to implement a class called rational number containing four 
+integer fields (Sign, numerator, denominator, exponent (raised to the power of 10)). 
+Provide operations for addition, subtraction and multiplication. Keep always the 
+values in lowest terms (Example: 8/12, should be represented as 2/3). Write a 
+private method for conversion to lowest terms. Also provide input and output 
+methods. */

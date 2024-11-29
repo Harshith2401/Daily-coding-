0bd765a2c21d2f39e7662fd8baa5d94646ffc2e1
Harshith2401 <orugantiharshith@gmail.com>
2024-11-29 23:41:50 +0530

    there should be no adjecent flowers

---
+++ flowerbed.java
@@ -0,0 +1,41 @@
+/*605. Can Place Flowers
+Solved
+Easy
+Topics
+Companies
+You have a long flowerbed in which some of the plots are planted, and some are not. However, flowers cannot be planted in adjacent plots.
+
+Given an integer array flowerbed containing 0's and 1's, where 0 means empty and 1 means not empty, and an integer n, return true if n new flowers can be planted in the flowerbed without violating the no-adjacent-flowers rule and false otherwise.
+
+ 
+
+Example 1:
+
+Input: flowerbed = [1,0,0,0,1], n = 1
+Output: true
+Example 2:
+
+Input: flowerbed = [1,0,0,0,1], n = 2
+Output: false
+ 
+
+Constraints:
+
+1 <= flowerbed.length <= 2 * 104
+flowerbed[i] is 0 or 1.
+There are no two adjacent flowers in flowerbed.
+0 <= n <= flowerbed.length */
+class Solution {
+    public boolean canPlaceFlowers(int[] flowerbed, int n) {
+        int flowerbedSize = flowerbed.length;
+        for (int i = 0; i < flowerbedSize && n > 0; i++) {
+            boolean prev = i == 0 || flowerbed[i - 1] == 0;
+            boolean fut = i == flowerbedSize - 1 || flowerbed[i + 1] == 0;
+            if (prev && fut && flowerbed[i] == 0) {
+                flowerbed[i] = 1;
+                n--;
+            }
+        }
+        return n <= 0;
+    }
+}

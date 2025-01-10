04008dae9db473ae39629a4d1d66134ddef98f28
Harshith2401 <orugantiharshith@gmail.com>
2025-01-11 00:16:51 +0530

    product of array

---
+++ productofarray.java
@@ -0,0 +1,54 @@
+/*Products of Array Except Self
+Solved 
+Given an integer array nums, return an array output where output[i] is the product of all the elements of nums except nums[i].
+
+Each product is guaranteed to fit in a 32-bit integer.
+
+Follow-up: Could you solve it in 
+O
+(
+n
+)
+O(n) time without using the division operation?
+
+Example 1:
+
+Input: nums = [1,2,4,6]
+
+Output: [48,24,12,8]
+Example 2:
+
+Input: nums = [-1,0,1,2,3]
+
+Output: [0,-6,0,0,0]
+Constraints:
+
+2 <= nums.length <= 1000
+-20 <= nums[i] <= 20 */
+
+public class Solution {
+    public int[] productExceptSelf(int[] nums) {
+        int prod = 1, zeroCount = 0;
+        for (int num : nums) {
+            if (num != 0) {
+                prod *= num;
+            } else {
+                zeroCount++;
+            }
+        }
+        
+        if (zeroCount > 1) {
+            return new int[nums.length]; 
+        }
+
+        int[] res = new int[nums.length];
+        for (int i = 0; i < nums.length; i++) {
+            if (zeroCount > 0) {
+                res[i] = (nums[i] == 0) ? prod : 0;
+            } else {
+                res[i] = prod / nums[i];
+            }
+        }
+        return res;
+    }
+}

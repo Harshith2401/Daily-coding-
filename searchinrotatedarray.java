5d35ce2749f1b2819d740387988435507c44e1bf
Harshith2401 <orugantiharshith@gmail.com>
2025-01-27 23:50:50 +0530

    searching a target value in rotated arry

---
+++ searchinrotatedarray.java
@@ -0,0 +1,64 @@
+/*Search in Rotated Sorted Array
+Solved 
+You are given an array of length n which was originally sorted in ascending order. It has now been rotated between 1 and n times. For example, the array nums = [1,2,3,4,5,6] might become:
+
+[3,4,5,6,1,2] if it was rotated 4 times.
+[1,2,3,4,5,6] if it was rotated 6 times.
+Given the rotated sorted array nums and an integer target, return the index of target within nums, or -1 if it is not present.
+
+You may assume all elements in the sorted rotated array nums are unique,
+
+A solution that runs in O(n) time is trivial, can you write an algorithm that runs in O(log n) time?
+
+Example 1:
+
+Input: nums = [3,4,5,6,1,2], target = 1
+
+Output: 4
+Example 2:
+
+Input: nums = [3,5,6,0,1,2], target = 4
+
+Output: -1
+Constraints:
+
+1 <= nums.length <= 1000
+-1000 <= nums[i] <= 1000
+-1000 <= target <= 1000 */
+public class Solution {
+    public int search(int[] nums, int target) {
+        int l = 0, r = nums.length - 1;
+
+        while (l < r) {
+            int m = (l + r) / 2;
+            if (nums[m] > nums[r]) {
+                l = m + 1;
+            } else {
+                r = m;
+            }
+        }
+
+        int pivot = l;
+
+        int result = binarySearch(nums, target, 0, pivot - 1);
+        if (result != -1) {
+            return result;
+        }
+
+        return binarySearch(nums, target, pivot, nums.length - 1);
+    }
+
+    public int binarySearch(int[] nums, int target, int left, int right) {
+        while (left <= right) {
+            int mid = (left + right) / 2;
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return -1;
+    }
+}

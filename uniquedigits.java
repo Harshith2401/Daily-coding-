935683e4368b6e6fb80020b30382be1b35823d18
Harshith2401 <orugantiharshith@gmail.com>
2025-02-24 10:00:19 +0530

    the brute force method for the solution

--- uniquedigits.java
+++ uniquedigits.java
@@ -1,3 +1,21 @@
+/*/Given an integer n, return the count of all numbers with unique digits, x, where <= x < 10n.
+
+Example 1:
+
+Input: n = 2
+
+Output: 91
+
+Explanation: The answer should be the total numbers in the range of s<100, excluding 11,22,33,44,55,66,77,88,99
+
+Example 2:
+
+Input: n=0
+
+Output: 1
+
+Constraints: 0 < n<=8 */
+
 import java.util.Scanner;
 
 public class uniquedigits{
